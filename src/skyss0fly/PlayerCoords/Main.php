0bf5eaa99dfad6939f9c8ab3be1ad64364a7e8fa
skyss0fly <100278495+skyss0fly@users.noreply.github.com>
2023-07-06 20:06:15 +1000

    Update and rename Main.php to Main.php

---
+++ src/skyss0fly/PlayerCoords/Main.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace skyss0fly\PlayerCoords;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerJoinEvent;
+
+class Main extends PluginBase implements Listener {
+
+    public function onEnable(): void {
+        $this->saveDefaultConfig();
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+    }
+
+    public function onPlayerJoin(PlayerJoinEvent $event) {
+        $player = $event->getPlayer();
+        $coords = $player->getCoordinates()
+        $message
+        $player->()->sendMessage($message);
+    }
+}
