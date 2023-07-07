04f2f0cae0c86ea9ae290920480a988d5d54e230
skyss0fly <100278495+skyss0fly@users.noreply.github.com>
2023-07-08 08:28:32 +1000

    Update reload.php

--- src/skyss0fly/PlayerCoords/command/reload.php
+++ src/skyss0fly/PlayerCoords/command/reload.php
@@ -4,5 +4,23 @@ namespace skyss0fly\PlayerCoords;
 
 use pocketmine\plugin\PluginBase;
 use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
 
-class reload extends 
+class reload extends PluginBase {
+public function onCommand(CommandSender $sender, Command $command, string $label, array $args) : bool{
+        if(!$sender instanceof Player){
+            $sender->sendMessage("Please Use this command in game!");
+            return false;
+        }
+    
+        switch($command->getName()){
+            case "reloadcoords":
+         
+                $this->getServer()->broadcastMessage("§l§e+§d[§fPlayerCoords§d]§e+ §r§eSuccessfully reloaded!);
+                return true;
+            default:
+                throw new \AssertionError("This line will never be executed");
+        
+        }
+
+}
