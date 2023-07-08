ca436f55c5690f662de997333e26fbe2013679ac
skyss0fly <100278495+skyss0fly@users.noreply.github.com>
2023-07-08 13:19:33 +1000

    Update Main.php

--- src/skyss0fly/PlayerCoords/Main.php
+++ src/skyss0fly/PlayerCoords/Main.php
@@ -8,44 +8,42 @@ use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
 use pocketmine\player\Player;
 use pocketmine\world\Position;
+
 class Main extends PluginBase implements Listener {
-  public function onEnable(): void {
+    public function onEnable(): void {
         $this->saveDefaultConfig();
         $this->getServer()->getPluginManager()->registerEvents($this, $this);
-    $this->getLogger()->info("§l§e+§d[§fPlayerCoords§d]§e+ §r§esuccessfully loaded and enabled, have a good day!");
+        $this->getLogger()->info("§l§e+§d[§fPlayerCoords§d]§e+ §r§esuccessfully loaded and enabled, have a good day!");
     }
 
-
-
- public function onCommand(CommandSender $sender, Command $command, string $label, array $args): bool{
-        if(!$sender instanceof Player){
-         $this->getLogger()->warning("Please use this command in game");
-               
+    public function onCommand(CommandSender $sender, Command $command, string $label, array $args): bool {
+        if (!$sender instanceof Player) {
+            $this->getLogger()->warning("Please use this command in-game");
             return false;
         }
+
         $x = $sender->getPosition()->getX();
         $y = $sender->getPosition()->getY();
         $z = $sender->getPosition()->getZ();
+
         $color = $this->getConfig()->get("ColorMode");
         $xcolorraw = $this->getConfig()->get("X");
-  $xcolor = str_replace("&", "§", $xcolorraw);
+        $xcolor = str_replace("&", "§", $xcolorraw);
         $ycolorraw = $this->getConfig()->get("Y");
-   $ycolor = str_replace("&", "§", $ycolorraw);
+        $ycolor = str_replace("&", "§", $ycolorraw);
         $zcolorraw = $this->getConfig()->get("Z");
-   $zcolor = str_replace("&", "§", $zcolorraw);
-  $r = "§r";
-        
-        switch($command->getName()){
+        $zcolor = str_replace("&", "§", $zcolorraw);
+        $r = "§r";
+
+        switch ($command->getName()) {
             case "coords":
-           if ($color){
-                $sender->sendMessage("Coordinates: " . "X: " .$xcolor . $x . $r .  ", " . "Y: " . $ycolor . $y . $r . ", " . "Z: " . $zcolor . $z);
-               
+                if ($color) {
+                    $sender->sendMessage("Coordinates: " . "X: " . $xcolor . $x . $r .  ", " . "Y: " . $ycolor . $y . $r . ", " . "Z: " . $zcolor . $z);
+                } else {
+                    $sender->sendMessage("Coordinates: " . "X: " . $x . ", " . "Y: "  . $y . ", " . "Z: " . $z);
+                }
                 return true;
-              }
-            else {
-              $sender->sendMessage("Coordinates: " . "X: " . $x . ", " . "Y: "  . $y . ", " . "Z: " . $z);
-       }
-       }
-       }
-       }
- 
+        }
+        return false;
+    }
+}
