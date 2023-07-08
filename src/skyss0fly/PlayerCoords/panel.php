ec145d197ccb4ab3dbfa24d274fd59ff01219742
skyss0fly <100278495+skyss0fly@users.noreply.github.com>
2023-07-08 20:31:17 +1000

    Update panel.php

--- src/skyss0fly/PlayerCoords/panel.php
+++ src/skyss0fly/PlayerCoords/panel.php
@@ -1,32 +1,29 @@
 <?php
 
-use
+namespace skyss0fly\PlayerCoords;
 
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\player\Player;
+class panel {
+public function onCommand(CommandSender $sender, Command $command, string $label, array $args): bool {
+        if (!$sender instanceof Player) {
+            $this->getLogger()->warning("Please use this command in-game");
+            return false;
+        }
 
-
-
-
- $formlol = new SimpleForm(function (Player $player, $data){
-            $result = $data;
-            if ($result !== null) {
-        
-                switch ($result) {
-                    case 0:
-                  
-    
-                    
-                      
-                    break;
-                }
-            }
-        });
-        $formtitle = $this->getConfig()->get("Admin");
-        $formcontent = $this->getConfig()->get("Select your module");
-    
-       
-        $formlol->setTitle($formtitle);
-        $formlol->setContent($formcontent);
-        $formlol->addButton("§d§lSubmit!");
-        $formlol->addButton("§d§lColorMode");
-        $player->sendForm($formlol);
-    }
+     
+        switch ($command->getName()) {
+            case "panel":
+               $colormode = $this->getConfig()->get("ColorMode");
+         if ($colormode){
+$sender->sendMessage("ColorMode Disabled");
+            file_put_contents($colormode,"False");
+         }
+         $sender->sendMessage("ColorMode Enabled");
+     file_put_contents($colormode,"True");
+        return false;
+}
+}
