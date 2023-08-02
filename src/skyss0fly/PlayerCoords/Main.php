753caa3c1170cf1790f8f1dca159bbdd0c518770
skyss0fly <100278495+skyss0fly@users.noreply.github.com>
2023-08-02 19:32:22 +1000

    Update Main.php

--- src/skyss0fly/PlayerCoords/Main.php
+++ src/skyss0fly/PlayerCoords/Main.php
@@ -54,7 +54,7 @@ class Main extends PluginBase implements Listener {
        return true;
                 }
        
-        else (!$color){
+        else {
                     $server->broadcastMessage($sender . "Is broadcasting: Coordinates: " . "X: " . $x . ", " . "Y: "  . $y . ", " . "Z: " . $z);
                     return true;
                 }
